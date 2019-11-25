489b357e88afa457b068d3c52f11000f520c3991
Shrey Viradiya <shrey.viradiya.100@outlook.com>
2019-11-25 22:16:41 +0530

    Little testing of REGEX

---
+++ testing.java
@@ -0,0 +1,12 @@
+/**
+ * testing
+ */
+public class testing {
+
+    public static void main(String[] args) {
+        String s = "I'm     19";
+        System.out.println(s);
+        String s2 = s.replaceAll("\\s+"," ");
+        System.out.println(s);
+    }
+}
