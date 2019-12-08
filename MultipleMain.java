1117f3e0f50124e83b3b863c7f09e092f13a28f2
Shrey Viradiya <40735908+Shrey-Viradiya@users.noreply.github.com>
2019-12-08 14:58:45 +0530

    Create MultipleMain.java

---
+++ MultipleMain.java
@@ -0,0 +1,9 @@
+class MultipleMain {
+  public static void main(String[] args) {
+    System.out.println("Static Main with String args");
+  }
+
+  public static void main(int x){
+    System.out.println("Static Main with Integer arg");
+  }
+}
